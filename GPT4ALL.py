e4d20af5e1b4699b6bf38250fe8b3c0765a7e829
Roman <EVIL@SUMRAK.com>
2024-02-04 21:23:50 +0400

    ...

---
+++ GPT4ALL.py
@@ -0,0 +1,26 @@
+#importing the necessary libraries
+from langchain.chains import LLMChain
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.llms import GPT4All
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.callbacks.base import BaseCallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.vectorstores import FAISS
+import argparse
+import requests
+
+#define arguments for command line
+ap = argparse.ArgumentParser()
+ap.add_argument('url',help="URL of PDF file")
+ap.add_argument('question',help='question for LLM')
+ap.add_argument('mode',help='choose mode: cpu or cuda:0')
+
+
+args = vars(ap.parse_args())
+
+
+callback = BaseCallbackManager([StreamingStdOutCallbackHandler()])
+url=args['url']
+
+
